a1e16c805569ea65dbf8eb4e6db2d38a48c8bcb9
이지영 <goodljyok@naver.com>
2018-12-12 14:18:47 +0900

    Initial Commit

---
+++ JY_Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JY_Game
+//
+//  Created by 이지영 on 12/12/2018.
+//  Copyright © 2018 SwiftBook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
